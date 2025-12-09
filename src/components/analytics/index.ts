b00f87ad1f5ee55ccf7c38d921dd49201e923c1e
Romain Jeanniot <jeanniot.webdev@icloud.com>
2025-12-09 19:18:46 +0100

    Config GTMv3 + Cookiebot

---
+++ src/components/analytics/index.ts
@@ -0,0 +1,4 @@
+export { CookiebotScript } from './CookiebotScript';
+export { GoogleConsentMode } from './GoogleConsentMode';
+export { GoogleTagManager } from './GoogleTagManager';
+export { trackEvent } from './trackEvent'
